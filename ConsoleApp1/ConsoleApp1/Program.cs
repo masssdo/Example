a4486b5485c0f7b8e4b6dc33cbb7841a896777aa
Massimo <113889906+masssdo@users.noreply.github.com>
2022-09-19 10:40:36 +0200

    Add files via upload
    AWESOME CODE

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CIAO A TUTTI");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Ciao a persona {i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
